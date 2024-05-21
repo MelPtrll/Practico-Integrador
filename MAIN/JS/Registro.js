a51e339f2b64fe1274f8945439e5e1133ab6aa23
Melina P <melupetrillo@gmail.com>
2024-05-21 16:12:19 -0300

    Se arreglan errores que salieron en el validator

--- MAIN/JS/Registro.js
+++ MAIN/JS/Registro.js
@@ -1,11 +1,11 @@
 var open2 = document.getElementById('open2');
-var modal_container = document.getElementById('modal_container');
-var close = document.getElementById('close');
+var modal_container2 = document.getElementById('modal_container2');
+var close2 = document.getElementById('close2');
 
-open2.addEventListener ('click', () =>{modal_container.classList.add('show');
+open2.addEventListener ('click', () =>{modal_container2.classList.add('show2');
 });
 
-close.addEventListener('click', () => {
-    modal_container.classList.remove('show');
+close2.addEventListener('click', () => {
+    modal_container2.classList.remove('show2');
 });
 
